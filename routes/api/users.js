e3e1aabb1a45d18193192cb1f77af6281339ac77
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-08-30 14:29:15 -0700

    User Route JWT update

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 // getting our user model
@@ -45,7 +47,20 @@ router.post(
       await user.save();
 
       // Return the jsonwebtoken
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
