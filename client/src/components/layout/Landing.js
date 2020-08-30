c31cc1f86f45ef6facda5e8a16eeeac23689440d
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-08-30 16:35:15 -0700

    App cleanup

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>this is the landing page</h1>
+    </div>
+  );
+};
+
+export default Landing;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>this is the nav bar</div>;
+};
+
+export default Navbar;
