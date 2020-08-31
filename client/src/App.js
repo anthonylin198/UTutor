e95ce697352563fdcf8ee4a11c2a86c4c5decf5c
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-08-30 17:44:03 -0700

    signin page update

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import "./App.css";
 const App = () => (
   <Router>
     <Fragment>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={Login} />
       <Route exact path="/register" component={Register} />
     </Fragment>
   </Router>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -120,8 +120,8 @@ export default function SignInSide() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                <Link to="/" variant="body2">
+                  {"Don't have an account? Sign in"}
                 </Link>
               </Grid>
             </Grid>
