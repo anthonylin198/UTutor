39bacc51886b16d3393fdb2437319cf7c5d3b882
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-08-30 17:37:06 -0700

    Homepage update

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,13 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Home from "./components/home/Home";
 import "./App.css";
 
 const App = () => (
   <Router>
     <Fragment>
-      <Route exact path="/" component={Login} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/register" component={Register} />
     </Fragment>
   </Router>
