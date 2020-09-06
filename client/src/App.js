f4354a3f6fd4e5f9aa208d32718d064ca92ebdad
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-09-06 12:13:20 -0700

    set authentical token update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // import Navbar from "./components/layout/Navbar";
@@ -11,16 +11,27 @@ import Alert from "./components/layout/Alert";
 // import Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Alert />
-        <Route exact path="/" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Alert />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 export default App;
