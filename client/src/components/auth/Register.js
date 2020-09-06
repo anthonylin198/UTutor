a84c7e84aa7c8f059f6391ca8f85e1ade3735c28
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-09-06 11:26:46 -0700

    alerts setup

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -59,7 +59,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignInSide() {
+export default function Register() {
   const classes = useStyles();
 
   // setup hook
